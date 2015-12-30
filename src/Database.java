849158ca0ef5dd6af27d863b88435b5071746eaf
rene <rene@rene.rene>
2015-12-30 23:39:10 +0200

    täiendatud andmebaasiga suhtlemise osa lisatud testid

--- src/Database.java
+++ src/Database.java
@@ -44,33 +44,58 @@ public class Database {
         }
     }
 
+    private void teostaAndmebaasiMuudatusPrepared(PreparedStatement preparedStatement) {
+        try {
+            // Statement objekt on vajalik, et SQL_Login käsku käivitada
+            preparedStatement.executeUpdate();
+            preparedStatement.close(); // Statement tuleb samuti kinni panna nagu ka Connection.
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void registreeriKasutaja(String username, String password) {
         // Andmete sisestamiseks on käsk INSERT. Esimestes sulgudes on tulabad KUHU salvestada,
         // teistes sulgudes VALUES() on MIDA salvestada.
-        String sql = "INSERT INTO USERS (USERNAME, PASSWORD) VALUES ('"+username+"','"+password+"')";
-        teostaAndmebaasiMuudatus(sql);
+        //String sql = "INSERT INTO USERS (USERNAME, PASSWORD) VALUES ('"+username+"','"+password+"')";
+        String sql = "INSERT INTO USERS (USERNAME, PASSWORD) VALUES (?, ?)";
+        PreparedStatement stat = null;
+        try {
+            stat = conn.prepareStatement(sql);
+            stat.setString(1, username);
+            stat.setString(2, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        teostaAndmebaasiMuudatusPrepared(stat);
     }
 
     public boolean login(String username, String password) {
         try {
-            Statement stat = conn.createStatement();
-
+            String sql = "SELECT * FROM USERS WHERE USERNAME = ? LIMIT 1;";
+            PreparedStatement stat = conn.prepareStatement(sql);
+            stat.setString(1, username);
             // SELECT on nagu excelis hiirega "selekteeriks" mingeid kaste. SQLis tähendab konkreetselt,
             // et milliste tulpade infot soovid kätte saada. WHERE'ga käsed välja võtta ainult tingimustele
             // vastavad väljad.
-            String sql = "SELECT * FROM USERS WHERE USERNAME = '" + username + "' LIMIT 1;";
+
 
             // Kuna tegu on päringuga siis käsuks on executeQuery ja ta tagastab andme objekti ResultSet.
-            ResultSet rs = stat.executeQuery(sql);
+            ResultSet rs = stat.executeQuery();
+            boolean isValid  = false;
+            while (rs.next()){
+                String dbPassword = rs.getString("password");
+                isValid = password.equals(dbPassword);
+                break;
+            }
 
             // Kui Query andmeid ei tagastanud (päring ei toonud tulemusi) siis rs-i kasutada ei saa.
             // Seepärast, kui kasutajat ei eksisteeri tuleb lihtsalt error ja "return" käsuni ei jõutagi.
             // Aga jõutakse lõpu "return false" käsuni küll.
-            String dbPassword = rs.getString("password");
 
             rs.close();
             stat.close();
-            return password.equals(dbPassword);
+            return isValid;
         } catch (SQLException e) {
             e.printStackTrace();
             System.exit(0);
@@ -93,21 +118,21 @@ public class Database {
     public HashMap getUser(String kasutajanimi) {
         HashMap andmed = new HashMap();
         try {
-            Statement stat = conn.createStatement();
-            String sql = "SELECT * FROM USERS WHERE USERNAME = '" + kasutajanimi + "' LIMIT 1;"; // LIMIT piirab tulemuste arvu.
-
-            ResultSet rs = stat.executeQuery(sql);
+            String sql = "SELECT * FROM USERS WHERE USERNAME = ? LIMIT 1;"; // LIMIT piirab tulemuste arvu.
+            PreparedStatement stat = conn.prepareStatement(sql);
+            stat.setString(1, kasutajanimi);
+            ResultSet rs = stat.executeQuery();
             // Kui stat.executeQuery() toob tagasi tühja tulemuse, siis rs'i kasutada ei saa.
 
             // Kui oleks mitu rida andmeid, peaks tsükliga lahendama while (rs.next()) {}
-
-            // Nopin käsitsi andmed ühelt realt välja
-            andmed.put("username", rs.getString("username"));
-            andmed.put("password", rs.getString("password"));
-            andmed.put("fullname", rs.getString("fullname"));
-            andmed.put("number", rs.getString("number"));
-            andmed.put("address", rs.getString("address"));
-
+            while(rs.next()){
+                andmed.put("username", rs.getString("username"));
+                andmed.put("password", rs.getString("password"));
+                andmed.put("fullname", rs.getString("fullname"));
+                andmed.put("number", rs.getString("number"));
+                andmed.put("address", rs.getString("address"));
+                break;
+            }
             rs.close();
             stat.close();
             return andmed;
@@ -122,18 +147,23 @@ public class Database {
         String username = andmed.get("username");
         String password = andmed.get("password");
         String fullname = andmed.get("fullname");
-        String number = andmed.get("number");
+        Integer number = Integer.parseInt(andmed.get("number"));
         String address = andmed.get("address");
 
+        String sql = "UPDATE USERS SET USERNAME = ?, PASSWORD = ?, FULLNAME = ?, NUMBER = ?, ADDRESS = ? WHERE USERNAME = ?;";
+        PreparedStatement stat = null;
         try {
-            Statement stat = conn.createStatement();
-            // Andmete uuendamise käsi on UPDATE. SET ütleb, et nüüd tulevad need uued andmed sisse. WHERE ütleb mis ridu uuendada.
-            String sql = String.format("UPDATE USERS SET USERNAME = '%s', PASSWORD = '%s', FULLNAME = '%s', NUMBER = '%s', ADDRESS = '%s' WHERE USERNAME = '%s';", username, password, fullname, number, address, username);
-            stat.executeUpdate(sql);
-            stat.close();
+            stat = conn.prepareStatement(sql);
+            stat.setString(1, username);
+            stat.setString(2, password);
+            stat.setString(3, fullname);
+            stat.setInt(4, number);
+            stat.setString(5, address);
+            stat.setString(6, username);
         } catch (SQLException e) {
             e.printStackTrace();
             System.exit(0);
         }
+        teostaAndmebaasiMuudatusPrepared(stat);
     }
 }
---
+++ src/test/DatabaseTest.java
@@ -0,0 +1,52 @@
+package src.test;
+
+import org.junit.Test;
+import src.Database;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rene on 30.12.2015.
+ */
+public class DatabaseTest {
+
+    @Test
+    public void testGetUser() throws Exception {
+        Database database = new Database();
+        HashMap user = database.getUser("test");
+        System.out.println("password=" + user.get("password"));
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        Database database = new Database();
+        boolean login = database.login("test", "p");
+        assertEquals(true, login);
+    }
+
+    @Test
+    public void testRegistreeriKasutaja() throws Exception {
+        String user = "reg";
+        String pass = "kas";
+        Database database = new Database();
+        database.registreeriKasutaja(user, pass);
+        boolean login = database.login(user, pass);
+        assertEquals(true, login);
+    }
+
+    @Test
+    public void testUuendaKasutajaAndmeid() throws Exception {
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("username", "test");
+        data.put("password", "test2");
+        data.put("fullname", "full");
+        data.put("number", "1");
+        data.put("address", "adr");
+        Database database = new Database();
+        database.uuendaKasutajaAndmeid(data);
+        boolean login = database.login("test", "test2");
+        assertEquals(true, login);
+    }
+}
